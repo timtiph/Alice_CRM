064659b539d80e706a52b0ac27bbd66d1db4181c
timtiph <tiphany.moine@outlook.com>
2023-03-15 16:23:54 +0100

    Creation of registration form

---
+++ src/Controller/RegisterController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\RegisterType;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ManagerRegistry as PersistenceManagerRegistry ;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RegisterController extends AbstractController
+{
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        // create entityManager => doctrine manager used for manipulation BDD
+        $this->entityManager = $entityManager;
+    }
+
+
+    #[Route('/inscription', name: 'app_register')]
+    public function index(Request $request, PersistenceManagerRegistry $doctrine, UserPasswordHasherInterface $passwordHasher): Response
+    {
+        // Instance Nouveau User, liée au formulaire registerType pour la création d'un User
+        $user = new User();
+
+        $form = $this->createForm(RegisterType::class, $user);
+
+        $form->handleRequest($request);
+        
+        if ($form->isSubmitted() && $form->isValid()) { 
+            $user = $form->getData();  // injecte dans obj $user toutes les données récup dans $form
+            
+            // vérifier la présence du mail saisie pour éviter les doublons
+            $search_email = $this->entityManager->getRepository(User::class)->findOneByEmail($user->getEmail());
+            
+            if(!$search_email) {
+                $password = $passwordHasher->hashPassword($user, $user->getPassword()); // hash le password saisi
+                $user->setPassword($password); // envoi le password dans obj User
+                
+                // dd($password);
+                // dd($user);
+                
+                //return entity manager
+                $entityManager = $doctrine->getManager();
+                $entityManager->persist($user); //figer les données 
+                $entityManager->flush(); // push les données
+
+                // notification de confirme inscription ok passée à la vue 
+                
+            }
+            
+            
+        }
+
+
+        return $this->render('register/index.html.twig', [
+            'form' => $form->createView()
+            
+        ]);
+    }
+}
---
+++ src/Form/RegisterType.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class RegisterType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('firstname', TextType::class, [
+                'label' => 'Votre Prénom',
+                'attr' => [
+                    'placeholder' => 'John'
+                ]
+            ])
+            ->add('lastname', TextType::class, [
+                'label' => 'Votre Nom',
+                'attr' => [
+                    'placeholder' => 'Doe'
+                ]
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Votre Email',
+                'attr' => [
+                    'placeholder' => 'example@mail.com'
+                ]
+            ])
+
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'Votre mot de passe et la confirmation doivent être identiques',
+                'label' => 'Votre mot de passe',
+                'required' => true,
+                'first_options' => [
+                    'label' => 'Mot de passe',
+                    'attr' => [
+                        'placeholder' => 'Merci de saisir votre mot de passe'
+                    ]
+                ],
+                'second_options' => [
+                    'label' => 'Confirmez votre mot de passe',
+                    'attr' => [
+                        'placeholder' => 'Merci de confirmer votre mot de passe'
+                    ]
+                ]
+            ])
+
+
+            ->add('submit', SubmitType::class, [
+                'label' => 'S\'inscrire'
+            ])
+        ;
+    }
+
+    // Ajout de contraintes à définir : https://symfony.com/doc/current/validation.html#constraints
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
