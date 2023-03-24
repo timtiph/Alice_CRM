405265331d46c4bf84a21a2d96a2e703fca6b46d
timtiph <tiphany.moine@outlook.com>
2023-03-24 18:59:31 +0100

    modif mise uses

--- src/Controller/AdminMainController.php
+++ src/Controller/AdminMainController.php
@@ -2,18 +2,18 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use App\Entity\Contact;
 use App\Entity\Customer;
-use App\Entity\User;
 use App\Form\CustomerType;
 use App\Form\EditUserType;
 use Doctrine\ORM\EntityManagerInterface;
-use Doctrine\Persistence\ManagerRegistry as PersistenceManagerRegistry;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Session\Flash\FlashBag;
+use Doctrine\Persistence\ManagerRegistry as PersistenceManagerRegistry;
+
 
 #[Route('/admin')]
 
--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -5,11 +5,12 @@ namespace App\Controller;
 use App\Entity\Contact;
 use App\Form\ContactType;
 use Doctrine\ORM\EntityManagerInterface;
-use Doctrine\Persistence\ManagerRegistry as PersistenceManagerRegistry;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\Persistence\ManagerRegistry as PersistenceManagerRegistry;
+
 
 class ContactController extends AbstractController
 {
