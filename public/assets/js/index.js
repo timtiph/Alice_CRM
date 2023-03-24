2ca01c3bbe9d9252581eef96fc1fc6ab82b1a8b4
timtiph <tiphany.moine@outlook.com>
2023-03-24 09:05:50 +0100

    css + js

---
+++ public/assets/js/index.js
@@ -0,0 +1,18 @@
+"use strict";
+ 
+
+let checkbox = document.getElementById("customer_isProfessional");
+let champ = document.getElementById("fieldCustomerSiret");
+ 
+if (checkbox) {
+  
+  checkbox.addEventListener("change", function () {
+    if (this.checked) {
+      champ.style.display = "block";
+    }
+    else {
+      champ.style.display = "none";
+    }
+  });
+
+}
