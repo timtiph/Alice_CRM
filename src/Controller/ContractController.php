413fa3f14ffb5c8b77349fce3f0fe9f7f70a18cd
timtiph <tiphany.moine@outlook.com>
2023-03-31 15:22:47 +0200

    Edit Contract / Show Contracts

--- src/Controller/ContractController.php
+++ src/Controller/ContractController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Contract;
 use App\Form\ContractType;
+use App\Form\EditContractType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -31,16 +32,15 @@ class ContractController extends AbstractController
         ]);
     }
 
-    // #[Route('/admin/contrat/{id}', name: 'app_contract')]
-    // public function showContract($id): Response
-    // {
-    //     $contract = $this->entityManager->getRepository(Contract::class)->findOneById($id);
+    #[Route('/admin/contrat/{id}', name: 'app_contract_show')]
+    public function showContract($id): Response
+    {
+        $contract = $this->entityManager->getRepository(Contract::class)->findOneById($id);
 
-    //     return $this->render('admin_main/contract_show.html.twig', [
-    //         'contract' => $contract
-    //     ]);
-
-    // }
+        return $this->render('admin_main/contract_show.html.twig', [
+            'contract' => $contract
+        ]);
+    }
 
     #[Route('/admin/contrat/creer-un-contrat', name: 'app_contract_add')]
     public function createContract(Request $request, EntityManagerInterface $entityManager, PersistenceManagerRegistry $doctrine): Response
@@ -74,6 +74,39 @@ class ContractController extends AbstractController
             'form' => $form->createView(), 
             'flash' => $this,
             'customer' => $contract->getCustomer(),
+            'user' => $contract->getCustomer()->getUser()
+        ]);
+    }
+
+    #[Route('/admin/contrat/modifier-un-contrat/{id}', name: 'app_contract_edit')]
+    public function editContract(Request $request, $id): Response
+    {
+        $contract = $this->entityManager->getRepository(Contract::class)->findOneById($id);
+        $customer = $contract->getCustomer();
+        $slug = $customer->getSlug();
+        $user = $customer->getUser();
+        $userId = $user->getId();
+
+        if (!$contract) {
+            return $this->redirectToRoute('app_customer_list'); 
+        }
+
+        $form = $this->createForm(EditContractType::class, $contract);
+
+
+        $form->handleRequest($request);
+        
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->entityManager->flush();
+            return $this->redirectToRoute('app_contract_show', array('id' => $id));
+        }
+
+        return $this->render('admin_main/contract_edit.html.twig', [
+            'contract' => $contract,
+            'form' => $form->createView(),
+            'user' => $user,
+            'customer' => $customer,
+            'id' => $userId
         ]);
     }
 }
