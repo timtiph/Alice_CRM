b656dd2bd9792a377b0339e04451216910e51b96
timtiph <tiphany.moine@outlook.com>
2023-04-27 12:25:40 +0200

    EmailVerify verification before validation of the connection

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -2,7 +2,8 @@
 
 namespace App\Security;
 
-use Symfony\Bundle\SecurityBundle\Security;
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\RedirectResponse;
@@ -14,6 +15,7 @@ use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
 use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
 use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
 
 class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
@@ -22,13 +24,22 @@ class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
 
     public const LOGIN_ROUTE = 'app_login';
 
-    public function __construct(private UrlGeneratorInterface $urlGenerator)
+    public function __construct(private UrlGeneratorInterface $urlGenerator, private EntityManagerInterface $entityManager)
     {
     }
 
     public function authenticate(Request $request): Passport
     {
+        
         $email = $request->request->get('email', '');
+
+        $user = $this->entityManager->getRepository(User::class)->findOneBy(['email' => $email]);
+
+        
+        if (!$user->getIsVerified()) {
+            // Email not verified, redirect user to verification page with an error message
+            throw new CustomUserMessageAuthenticationException('Votre compte n\'est pas vérifié. Veuillez lire le mail qui vous a été envoyé lors de votre inscription et suivre les indications. (Pensez à vérifier vos spams.)<br> Vous pouvez aussi <a href="mailto:alice.webdeveloppement@gmail.com?subject=' . rawurlencode('Problème avec la vérification de l\'email de ' . $user->getFirstname() ." ". $user->getLastname()) . '">écrire à Antoine.</a>');
+        }
 
         //$request->getSession()->set(Security::LAST_USERNAME, $email); // => généré automatiquement mais déprécié par Intelephense
         $request->getSession()->set(SecurityBundleSecurity::LAST_USERNAME, $email); // => correction ?
