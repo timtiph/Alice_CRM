5af2979557ad97a7b851dbef8a1ba6e3e488138b
timtiph <tiphany.moine@outlook.com>
2023-05-06 07:07:02 +0200

    remove a use not used

--- src/Repository/DocumentRepository.php
+++ src/Repository/DocumentRepository.php
@@ -2,7 +2,6 @@
 
 namespace App\Repository;
 
-use App\Entity\User;
 use App\Entity\Document;
 use Doctrine\Persistence\ManagerRegistry;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
